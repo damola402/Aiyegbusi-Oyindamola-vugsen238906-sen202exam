38d6f6f77a50323ea1074208a5c3c7ca6167824a
damola402 <111243844+damola402@users.noreply.github.com>
2025-06-28 10:32:28 +0100

    created serializer for the address model

--- damola_staff_api/employees/serializers.py
+++ damola_staff_api/employees/serializers.py
@@ -1,11 +1,16 @@
+
 from rest_framework import serializers
-from .models import Manager, Intern
+from .models import Manager, Intern, Address
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
 
 class ManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Manager
-        fields = '__all__'
-        extra_kwargs = {'has_company_card': {'read_only': True}}
+        exclude = ['has_company_card']  # encapsulation
 
 class InternSerializer(serializers.ModelSerializer):
     class Meta:
