e2b7beadc804007d2fdca84b6155d8d310eb4a22
damola402 <111243844+damola402@users.noreply.github.com>
2025-06-28 10:33:57 +0100

    question 3 created a reuseable address model and asscociated with manager and intern

--- damola_staff_api/employees/models.py
+++ damola_staff_api/employees/models.py
@@ -1,10 +1,18 @@
+
 from django.db import models
 
-class StaffBase(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    date_joined = models.DateField(auto_now_add=True)
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
 
+class StaffBase(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
+    
     class Meta:
         abstract = True
 
@@ -19,7 +27,7 @@ class Manager(StaffBase):
         return "Manager"
 
 class Intern(StaffBase):
-    mentor = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    mentor = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True)
     internship_end = models.DateField()
 
     def get_role(self):
